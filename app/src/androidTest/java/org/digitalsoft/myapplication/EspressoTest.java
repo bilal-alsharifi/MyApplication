9d578edddc6e8a850a178665dcaa4bc216e87f90
Bilal Alsharifi <bilal.alsharifi@gmail.com>
2019-08-12 23:11:12 -0400

    initial commit

---
+++ app/src/androidTest/java/org/digitalsoft/myapplication/EspressoTest.java
@@ -0,0 +1,35 @@
+package org.digitalsoft.myapplication;
+
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.CoreMatchers.not;
+
+@RunWith(AndroidJUnit4.class)
+public class EspressoTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testMainFlow() {
+        onView(withId(R.id.button))
+                .check(matches(isEnabled()));
+
+        onView(withId(R.id.button))
+                .perform(click());
+
+        onView(withId(R.id.button))
+                .check(matches(not(isEnabled())));
+
+    }
+}
